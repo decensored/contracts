09c95c3c7fb665737fd40e662797c1edca898b7d
Lukas Tassanyi <microhash@twitter.com>
2022-01-30 11:52:37 -0600

    rename ABIs

--- scripts/generate-abis.sh
+++ scripts/generate-abis.sh
@@ -6,8 +6,9 @@ npx solcjs --abi --include-path node_modules/ --base-path ./ ./contracts/Posts.s
 
 rm abis/@openzeppelin* abis/hardhat_console_sol_console.abi
 
-mv abis/contracts_Accounts_sol_Accounts.abi       abis/contracts_Accounts_sol_Accounts.json
-mv abis/contracts_Posts_sol_Posts.abi             abis/contracts_Posts_sol_Posts.json
-mv abis/contracts_RateControl_sol_RateControl.abi abis/contracts_RateControl_sol_RateControl.json
-mv abis/contracts_Spaces_sol_Spaces.abi           abis/contracts_Spaces_sol_Spaces.json
-
+mv abis/contracts_Accounts_sol_Accounts.abi       abis/ABI_Accounts.json
+mv abis/contracts_Posts_sol_Posts.abi             abis/ABI_Posts.json
+mv abis/contracts_RateControl_sol_RateControl.abi abis/ABI_RateControl.json
+mv abis/contracts_Spaces_sol_Spaces.abi           abis/ABI_Spaces.json
+mv abis/contracts_Contracts_sol_Contracts.abi     abis/ABI_Contracts.json
+rm abis/contracts_Tokens_sol_Tokens.abi
